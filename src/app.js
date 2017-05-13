15dd27b49995e3af5ca8c513d96f566876e54c5b
Jay Letheby <jay.letheby@gmail.com>
2017-05-13 22:11:04 +1000

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Header } from './Components/Common';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+      </View>
+    );
+  }
+}
+
+export default App;
