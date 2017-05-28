af993e7284515a6a8839d67f54cacefeac745e38
Jay Letheby <jay.letheby@gmail.com>
2017-05-28 22:18:56 +1000

    added login form

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './Components/Common';
+import LoginForm from './Components/LoginForm';
 
 class App extends Component {
+  componentWillMount(){
+      firebase.initializeApp({
+        apiKey: "AIzaSyCYZaFO05e7RdesSUNAPm3703QFrkwSKu0",
+        authDomain: "auth-98fef.firebaseapp.com",
+        databaseURL: "https://auth-98fef.firebaseio.com",
+        projectId: "auth-98fef",
+        storageBucket: "auth-98fef.appspot.com",
+        messagingSenderId: "47683223280"
+      });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
